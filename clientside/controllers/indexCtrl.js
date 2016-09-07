c36a62dbcc94d6e4b9ba13a8eb4efb5c8cceaf74
carloseduardoolivera <carloseduardoolivera@gmail.com>
2016-09-06 21:55:57 -0300

    posts

--- clientside/controllers/indexCtrl.js
+++ clientside/controllers/indexCtrl.js
@@ -17,10 +17,10 @@ angular.module("main").controller("indexCtrl", function ($rootScope,$scope, $htt
 	        };
 
 
-$scope.findCharacters = function () {
+		$scope.findCharacters = function () {
 			$scope.onlineChars = [];
-		$http.get('http://localhost:5000/posts/').then(function (response) {
-		//$http.get('https://shrouded-refuge-17729.herokuapp.com/posts/').then(function (response) {
+		//$http.get('http://localhost:5000/posts/').then(function (response) {
+		$http.get('https://shrouded-refuge-17729.herokuapp.com/posts/').then(function (response) {
 			
 			delete $scope.posts;
 			$scope.posts = response.data;
