cbbd5c8624ee25331d483748d8f9e44bba717650
carloseduardoolivera <carloseduardoolivera@gmail.com>
2016-09-07 16:08:50 -0300

    ajustes

--- clientside/controllers/usereditionCtrl.js
+++ clientside/controllers/usereditionCtrl.js
@@ -6,31 +6,16 @@ angular.module("main").controller("usereditionCtrl", function ($rootScope, $scop
 		$scope.findUserToEdit = function(login){
 			$http.get('https://shrouded-refuge-17729.herokuapp.com/users/'+$rootScope.login).then(function (response) {
 			//$http.get('https://localhost:5000/users/'+$rootScope.login).then(function (response) {
+
 			$scope.userToEdit = response.data;
-			var login = params.login;
-			var passw = criptography.encode(params.password);
-
-			for(index in $scope.users){
-				if($scope.users[index].username.toUpperCase() == login.toUpperCase() ){
-					if($scope.users[index].password == passw){
-						$rootScope.login = login;
-						$rootScope.logged = true;
-						$rootScope.menu = true;
-						var nameParts = ($scope.users[index].name).split(" ");
-						$rootScope.username = nameParts[0];
-						$scope.error = false;
-						return $location.path('/home');
-						//return $location.url('/home');
-					}
-				}
-
-			};
-			$scope.error = true;
-
+						//return $location.path('/home');
+					
 		});
 
 
 		};
+
+		$scope.findUserToEdit(login);
 
 
 			$scope.checkPassword = function(user){
